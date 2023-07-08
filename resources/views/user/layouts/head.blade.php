5af1524d4301ec9b04e23002c9c2bee90111235f
Rio Saputro <rio.ae23@gmail.com>
2023-07-08 17:33:32 +0700

    little fix

--- resources/views/user/layouts/head.blade.php
+++ resources/views/user/layouts/head.blade.php
@@ -6,22 +6,22 @@
 <meta content="" name="keywords">
 
 <!-- Favicons -->
-<link href="assets/img/favicon.png" rel="icon">
-<link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+<link href="assets/login/img/favicon.png" rel="icon">
+<link href="assets/login/img/apple-touch-icon.png" rel="apple-touch-icon">
 
 <!-- Google Fonts -->
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
 <!-- Vendor CSS Files -->
-<link href="assets/vendor/aos/aos.css" rel="stylesheet">
-<link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-<link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-<link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-<link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-<link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+<link href="assets/login/vendor/aos/aos.css" rel="stylesheet">
+<link href="assets/login/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="assets/login/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+<link href="assets/login/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+<link href="assets/login/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+<link href="assets/login/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
 <!-- Template Main CSS File -->
-<link href="assets/css/style.css" rel="stylesheet">
+<link href="assets/login/css/style.css" rel="stylesheet">
 
 <!-- =======================================================
 * Template Name: Knight
